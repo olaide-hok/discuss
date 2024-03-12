9d8342ebec73ff7fd6263f3368fe3caf2643f4c2
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-12 15:40:45 +0100

    Add authentication status validation

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -41,6 +41,12 @@ const PostCreateform = () => {
                             errorMessage={formState.errors.content?.join(', ')}
                         />
 
+                        {formState.errors._form ? (
+                            <div className="rounded p-2 bg-red-200 border border-red-400">
+                                {formState.errors._form?.join(', ')}
+                            </div>
+                        ) : null}
+
                         <FormButton>Create a Post</FormButton>
                     </div>
                 </form>
