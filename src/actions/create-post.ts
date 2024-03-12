a76e5c1e36d6a6ad5b4ce2ed251073823d3ac28a
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-12 15:51:49 +0100

    Receive slug prop and check the db for its topic id

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import {auth} from '@/auth'
+import {db} from '@/db'
 import {z} from 'zod'
 
 const createPostSchema = z.object({
@@ -17,6 +18,7 @@ interface CreatePostFormState {
 }
 
 export async function createPost(
+    slug: string,
     formState: CreatePostFormState,
     formData: FormData
 ): Promise<CreatePostFormState> {
@@ -36,6 +38,18 @@ export async function createPost(
         return {
             errors: {
                 _form: ['You must be signed in to do this!'],
+            },
+        }
+    }
+
+    const topic = await db.topic.findFirst({
+        where: {slug},
+    })
+
+    if (!topic) {
+        return {
+            errors: {
+                _form: ['Cannot find topic!'],
             },
         }
     }
