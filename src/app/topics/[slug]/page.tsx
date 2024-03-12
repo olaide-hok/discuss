890522766f7a47bc63f052e5954bf7f676c4e628
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-12 15:18:01 +0100

    Setup TopicShowPage cmp

---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import PostCreateform from '@/components/posts/post-create-form'
+
+interface TopicShowPageProps {
+    params: {
+        slug: string
+    }
+}
+
+const TopicShowPage = ({params}: TopicShowPageProps) => {
+    const {slug} = params
+
+    return (
+        <div className="grid grid-cols-4 gap-4 p-4">
+            <div className="col-span-3">
+                <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+            </div>
+
+            <div>
+                <PostCreateform />
+            </div>
+        </div>
+    )
+}
+
+export default TopicShowPage
