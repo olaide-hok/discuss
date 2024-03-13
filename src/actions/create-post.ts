2e36b25f9d69d349ea7f28ba8dce54c4f23337ca
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-13 09:25:48 +0100

    Add db querying to createPost server action

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -2,6 +2,10 @@
 
 import {auth} from '@/auth'
 import {db} from '@/db'
+import paths from '@/paths'
+import {Post} from '@prisma/client'
+import {revalidatePath} from 'next/cache'
+import {redirect} from 'next/navigation'
 import {z} from 'zod'
 
 const createPostSchema = z.object({
@@ -54,7 +58,33 @@ export async function createPost(
         }
     }
 
-    return {
-        errors: {},
+    let post: Post
+
+    try {
+        post = await db.post.create({
+            data: {
+                title: result.data.title,
+                content: result.data.content,
+                userId: session.user.id,
+                topicId: topic.id,
+            },
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                errors: {
+                    _form: [error.message],
+                },
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Failed to create post'],
+                },
+            }
+        }
     }
+
+    revalidatePath(paths.topicShow(slug))
+    redirect(paths.postShow(slug, post.id))
 }
