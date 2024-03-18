2d09203479edb4208a335eca856b550c2d779d21
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-18 12:06:59 +0100

    Complete PostShowPage

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,27 +1,30 @@
-import Link from "next/link";
-import PostShow from "@/components/posts/post-show";
-import CommentList from "@/components/comments/comment-list";
-import CommentCreateForm from "@/components/comments/comment-create-form";
-import paths from "@/paths";
+import Link from 'next/link'
+import PostShow from '@/components/posts/post-show'
+import CommentList from '@/components/comments/comment-list'
+import CommentCreateForm from '@/components/comments/comment-create-form'
+import paths from '@/paths'
+import {fetchCommentsByPostId} from '@/db/queries/comments'
 
 interface PostShowPageProps {
-  params: {
-    slug: string;
-    postId: string;
-  };
+    params: {
+        slug: string
+        postId: string
+    }
 }
 
-export default async function PostShowPage({ params }: PostShowPageProps) {
-  const { slug, postId } = params;
+export default async function PostShowPage({params}: PostShowPageProps) {
+    const {slug, postId} = params
 
-  return (
-    <div className="space-y-3">
-      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {"< "}Back to {slug}
-      </Link>
-      {/* <PostShow /> */}
-      {/* <CommentCreateForm postId={postId} startOpen /> */}
-      {/* <CommentList comments={comments} /> */}
-    </div>
-  );
+    return (
+        <div className="space-y-3">
+            <Link
+                className="underline decoration-solid"
+                href={paths.topicShow(slug)}>
+                {'< '}Back to {slug}
+            </Link>
+            <PostShow postId={postId} />
+            <CommentCreateForm postId={postId} startOpen />
+            <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+        </div>
+    )
 }
