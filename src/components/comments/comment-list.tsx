eb9877408bd0059c2811615b6e111c202a392cbb
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-18 15:48:54 +0100

    Make CommentList comp to use postId prop and fetchCommentsByPostId Fn

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,12 +1,43 @@
 import CommentShow from '@/components/comments/comment-show'
-import {CommentWithAuthor} from '@/db/queries/comments'
+import {CommentWithAuthor, fetchCommentsByPostId} from '@/db/queries/comments'
+
+// interface CommentListProps {
+//     fetchData: () => Promise<CommentWithAuthor[]>
+// }
+
+// export default async function CommentList({fetchData}: CommentListProps) {
+//     const comments = await fetchData()
+//     const topLevelComments = comments.filter(
+//         (comment) => comment.parentId === null
+//     )
+//     const renderedComments = topLevelComments.map((comment) => {
+//         return (
+//             <CommentShow
+//                 key={comment.id}
+//                 commentId={comment.id}
+//                 comments={comments}
+//             />
+//         )
+//     })
+
+//     return (
+//         <div className="space-y-3">
+//             <h1 className="text-lg font-bold">
+//                 All {comments.length} comments
+//             </h1>
+//             {renderedComments}
+//         </div>
+//     )
+// }
+
+// Another approach
 
 interface CommentListProps {
-    fetchData: () => Promise<CommentWithAuthor[]>
+    postId: string
 }
 
-export default async function CommentList({fetchData}: CommentListProps) {
-    const comments = await fetchData()
+export default async function CommentList({postId}: CommentListProps) {
+    const comments = await fetchCommentsByPostId(postId)
     const topLevelComments = comments.filter(
         (comment) => comment.parentId === null
     )
@@ -15,7 +46,7 @@ export default async function CommentList({fetchData}: CommentListProps) {
             <CommentShow
                 key={comment.id}
                 commentId={comment.id}
-                comments={comments}
+                postId={postId}
             />
         )
     })
